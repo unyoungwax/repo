6e24ae754f387f7c3c35f3796f0510b92ea8cc94
Tania Chattopadhyay <tchatterjee5@gmail.com>
2015-06-04 12:23:50 +0800

    Added VisibilityControl.java

---
+++ src/main/java/teammates/logic/api/VisibilityControl.java
@@ -0,0 +1,25 @@
+package teammates.logic.api;
+
+import teammates.common.datatransfer.CommentAttributes;
+import teammates.common.datatransfer.CommentParticipantType;
+import teammates.common.datatransfer.FeedbackParticipantType;
+import teammates.common.datatransfer.FeedbackResponseCommentAttributes;
+
+public class VisibilityControl {
+
+    public boolean isCommentPublicToRecipient(CommentAttributes comment) {
+        return comment.showCommentTo != null
+                && (comment.isVisibleTo(CommentParticipantType.PERSON)
+                    || comment.isVisibleTo(CommentParticipantType.TEAM)
+                    || comment.isVisibleTo(CommentParticipantType.SECTION)
+                    || comment.isVisibleTo(CommentParticipantType.COURSE));
+    }
+    
+    public boolean isResponseCommentPublicToRecipient(FeedbackResponseCommentAttributes comment) {
+        return (comment.isVisibleTo(FeedbackParticipantType.GIVER)
+             || comment.isVisibleTo(FeedbackParticipantType.RECEIVER)
+             || comment.isVisibleTo(FeedbackParticipantType.OWN_TEAM_MEMBERS)
+             || comment.isVisibleTo(FeedbackParticipantType.RECEIVER_TEAM_MEMBERS)
+             || comment.isVisibleTo(FeedbackParticipantType.STUDENTS));
+    }
+}
